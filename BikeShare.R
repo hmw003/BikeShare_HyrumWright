74e23ae259aafa23a43a1eae23641b6cd45e3135
hmw003 <96318644+hmw003@users.noreply.github.com>
2025-09-12 11:09:19 -0600

    Created graphs

--- BikeShare.R
+++ BikeShare.R
@@ -1,2 +1,21 @@
 library(tidyverse)
 library(tidymodels)
+library(patchwork)
+
+train<- vroom("train.csv")
+
+weather_plot <- ggplot(data = train, aes(x = weather)) +
+  geom_bar()
+
+temp_plot <- ggplot(data=train, aes(x=temp, y=count)) +
+  geom_point() +
+  geom_smooth(se = FALSE)
+
+workday_plot <- ggplot(data = train, aes(x = workingday)) +
+  geom_bar()
+
+windspeed_plot <- ggplot(train, aes(x= windspeed, y= count)) + 
+  geom_point()
+
+(weather_plot +windspeed_plot) / (temp_plot + workday_plot)
+  
