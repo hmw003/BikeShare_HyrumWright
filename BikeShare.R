d7399bea4207bc37b7a7a45eb9e2d16c35de8ffb
hmw003 <96318644+hmw003@users.noreply.github.com>
2025-10-02 10:15:31 -0600

    Updated code

--- BikeShare.R
+++ BikeShare.R
@@ -1,9 +1,63 @@
+# BikeShareAnalysis.R
+# Linear Regression for Kaggle Bike Sharing
+# -----------------------------------------
+
 library(tidyverse)
-library(tidymodels)
-library(patchwork)
+library(lubridate)
 library(vroom)
 
-bike<- vroom("train.csv")
+# --- Load data with vroom ---
+train <- vroom("train.csv", col_types = cols(datetime = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
+test  <- vroom("test.csv",  col_types = cols(datetime = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
+
+# --- Feature Engineering ---
+add_datetime_features <- function(df) {
+  df %>%
+    mutate(
+      hour       = hour(datetime),
+      day        = day(datetime),
+      month      = month(datetime),
+      year       = ifelse(year(datetime) == 2011, 0, 1),
+      dayofweek  = wday(datetime, week_start = 1) - 1, # Monday = 0
+      is_weekend = ifelse(dayofweek >= 5, 1, 0)
+    )
+}
+
+train <- add_datetime_features(train)
+test  <- add_datetime_features(test)
+
+# --- Target: log(count) ---
+train$log_count <- log1p(train$count)
+
+# --- Fit linear regression ---
+# (exclude casual and registered as predictors)
+lm_model <- lm(
+  log_count ~ season + weather + holiday + workingday +
+    temp + atemp + humidity + windspeed +
+    hour + day + month + year + dayofweek + is_weekend,
+  data = train
+)
+
+summary(lm_model)
+
+# --- Predict on test set ---
+test$pred_log <- predict(lm_model, newdata = test)
+test$pred     <- pmax(0, expm1(test$pred_log))  # back-transform and clip negatives
+
+# --- Build submission (fixed) ---
+submission <- test %>%
+  mutate(
+    datetime = format(datetime, "%Y-%m-%d %H:%M:%S"),
+    count = round(pred)
+  ) %>%
+  select(datetime, count)
+
+# --- Save submission with vroom_write ---
+vroom_write(submission, "submission.csv", delim = ",")
+
+cat("Submission saved to submission.csv\n")
+print(head(submission))
+
 
 weather_plot <- ggplot(data = bike, aes(x = weather)) +
   geom_bar()
@@ -19,29 +73,3 @@ windspeed_plot <- ggplot(bike, aes(x= windspeed, y= count)) +
   geom_point()
 
 (weather_plot +windspeed_plot) / (temp_plot + workday_plot)
-
-### HOMEWORK 2
-
-test <- vroom("test.csv", col_types = cols(datetime = col_character()))
-
-# Fit log-linear regression model
-my_linear_model <- linear_reg() %>%
-  set_engine("lm") %>%
-  set_mode("regression") %>%
-  fit(log(count) ~ season + holiday + workingday + weather + temp + atemp + humidity + windspeed,
-      data = bike)
-
-# Predict on test set
-log_predictions <- predict(my_linear_model, new_data = test)
-
-# Back-transform and clip negative counts
-bike_predictions <- pmax(exp(log_predictions$.pred), 0)
-
-# Prepare submission
-submission <- tibble(
-  datetime = test$datetime,
-  count = bike_predictions
-)
-
-# Save CSV
-vroom_write(submission, "submission.csv", delim = ",")
